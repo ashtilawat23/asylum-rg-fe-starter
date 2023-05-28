01a01190aafef16c7d9fb97d6dd5f1bffbf88222
Robby Lewis <Robbysim37@gmail.com>
2023-05-28 17:01:58 -0400

    finished styling user info

--- src/components/common/Profile.js
+++ src/components/common/Profile.js
@@ -4,7 +4,12 @@ const Profile = props => {
   return (
     <div
       className="user-info-container"
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '60vh',
+      }}
     >
       <div className="row align-items-center profile-header">
         <div className="col-md-2 mb-3">
@@ -12,11 +17,29 @@ const Profile = props => {
             src={props.user.picture}
             alt="Profile"
             className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+            style={{ borderRadius: '50%', width: '200px', margin: '30px' }}
           />
         </div>
         <div className="col-md text-center text-md-left">
-          <h2>{props.user.name}</h2>
-          <p className="lead text-muted">{props.user.email}</p>
+          <h2
+            style={{
+              textAlign: 'center',
+              fontSize: '40px',
+              fontFamily: 'MonetaBold',
+            }}
+          >
+            {props.user.name}
+          </h2>
+          <p
+            style={{
+              textAlign: 'center',
+              fontSize: '20px',
+              fontFamily: 'Moneta',
+            }}
+            className="lead text-muted"
+          >
+            {props.user.email}
+          </p>
         </div>
       </div>
     </div>
