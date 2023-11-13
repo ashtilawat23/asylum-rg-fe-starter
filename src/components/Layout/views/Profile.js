71f6f75ba1004a87fb2f8a90767c7ee322726b2d
Dalvis <Dalvis.liz27@gmail.com>
2023-11-13 15:07:08 -0500

    added bootstrap to the profile page

--- src/components/Layout/views/Profile.js
+++ src/components/Layout/views/Profile.js
@@ -1,29 +1,56 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Profile = () => {
   const { user } = useAuth0();
   const { name, picture, email } = user;
 
+  console.log(user);
+
   return (
     <div>
-      <div className="row align-items-center profile-header">
-        <div className="col-md-2 mb-3">
+      <div className="row align-items-center">
+        <div className="col-md-2 m-3">
           <img
             src={picture}
             alt="Profile"
             className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
           />
         </div>
-        <div className="col-md text-center text-md-left">
+        <div className="col-md text-left text-md-left">
           <h2>{name}</h2>
-          <p className="lead text-muted">{email}</p>
+          {email ? <p className="lead text-muted">{email}</p> : null}
         </div>
       </div>
       <div className="row">
-        <pre className="col-12 text-light bg-dark p-4">
-          {JSON.stringify(user, null, 2)}
+        <pre className="col-12  p-4">
+          <div className="col-md-6 ">
+            <table className="table table-striped table-hover table-bordered ">
+              <thead className="table-dark">
+                <tr>
+                  <th></th>
+                  <th className="text-center">Data</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(user).map((keyName, i) => (
+                  <tr key={i}>
+                    <td>
+                      {keyName
+                        .replace('_', ' ')
+                        .split(' ')
+                        .map(
+                          word => word.charAt(0).toUpperCase() + word.slice(1)
+                        )
+                        .join(' ')}
+                    </td>
+                    <td className="text-wrap">{user[keyName]}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </pre>
       </div>
     </div>
