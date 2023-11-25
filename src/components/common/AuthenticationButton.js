dd86315b39a1bc01fc695e6ad4b0df88947fe7e4
Dalvis <Dalvis.liz27@gmail.com>
2023-11-25 17:13:35 -0500

    added comments to AuthenticationButton.js

--- src/components/common/AuthenticationButton.js
+++ src/components/common/AuthenticationButton.js
@@ -8,7 +8,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 // switches between login and logout buttons depending on authentication status
 const AuthenticationButton = () => {
   const { isAuthenticated } = useAuth0();
-
+  // both buttons are almost the same, but the login button calls loginWithRedirect() and the logout button calls logout()
   return isAuthenticated ? <LogoutButton /> : <LoginButton />;
 };
 
