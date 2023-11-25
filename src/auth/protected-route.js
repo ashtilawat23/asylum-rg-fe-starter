519fc112512f0909f219e81c2df736d50a25c4f8
Dalvis <Dalvis.liz27@gmail.com>
2023-11-25 17:12:30 -0500

    added comments to the protectedRoute.js

--- src/auth/protected-route.js
+++ src/auth/protected-route.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 import { LoadingPage } from '../components/pages/Loading';
 
+// this will protect routes that require authentication
 const ProtectedRoute = ({ component, ...args }) => (
   <Route
     component={withAuthenticationRequired(component, {
