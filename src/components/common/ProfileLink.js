2306fe2f8d487e13c9ac2bb0719c215a8e0cc820
Dalvis <Dalvis.liz27@gmail.com>
2023-11-25 17:14:32 -0500

    added comments to ProfileLink.js

--- src/components/common/ProfileLink.js
+++ src/components/common/ProfileLink.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
+// this is a link to the profile page, this only shows up if the user is logged
+// otherwise it shows an empty profile avi
 export default function ProfileLink() {
   const { picture } = useAuth0().user;
   return (
