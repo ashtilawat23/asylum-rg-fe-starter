74e333492237a4002516a51718b2f7b5c12a35ff
cam <cjmooredev@gmail.com>
2024-08-04 21:05:33 -0400

    resolved authentication issues

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -1,6 +1,6 @@
 const config = {
   issuer: process.env.REACT_APP_OKTA_ISSUER_URI,
-  redirect_uri: window.location.origin + '/implicit/callback',
+  redirect_uri: window.location.origin,
   clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
   pkce: true,
   scopes: ['openid', 'email', 'profile'],
